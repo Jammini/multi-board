b80def87b833e41d35ac171fd82cb31e0de9dd96
Jammini <systemdata227@gmail.com>
2025-04-04 22:25:15 +0900

    [refactor] 테스트 코드 수정

--- src/test/java/study/multiproject/api/service/post/PostServiceTest.java
+++ src/test/java/study/multiproject/api/service/post/PostServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,8 @@ import study.multiproject.api.service.post.response.PagingResponse;
 import study.multiproject.api.service.post.response.PostResponse;
 import study.multiproject.domain.post.Post;
 import study.multiproject.domain.post.PostRepository;
+import study.multiproject.domain.user.User;
+import study.multiproject.domain.user.UserRepository;
 
 @WithMockUser(username = "test@example.com", roles = "USER")
 @Transactional
@@ -33,11 +36,25 @@ class PostServiceTest {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    @BeforeEach
+    void beforeEach() {
+        user = User.builder()
+                        .email("test@gmail.com")
+                        .password("1234")
+                        .name("잼미니")
+                        .build();
+        userRepository.save(user);
+    }
+
     @Test
     @DisplayName("신규 게시글을 작성하면 아이디를 반환한다.")
     void createPost() {
         // given
-        PostCreateServiceRequest request = new PostCreateServiceRequest("잼미니", "반포자이 살고싶다.", List.of("잼미니", "반포자이"), List.of(), 1L);
+        PostCreateServiceRequest request = new PostCreateServiceRequest("잼미니", "반포자이 살고싶다.", List.of("잼미니", "반포자이"), List.of(), user.getId());
 
         // when
         Long postId = postService.write(request);
@@ -56,6 +73,7 @@ class PostServiceTest {
         Post post = Post.builder()
                         .title("잼미니")
                         .content("한남더힐 살고싶다.")
+                        .user(user)
                         .build();
         postRepository.save(post);
 
@@ -75,10 +93,12 @@ class PostServiceTest {
         Post post1 = Post.builder()
                          .title("잼미니1")
                          .content("내용입니다1")
+                         .user(user)
                          .build();
         Post post2 = Post.builder()
                          .title("잼미니2")
                          .content("내용입니다2")
+                         .user(user)
                          .build();
         postRepository.saveAll(List.of(post1, post2));
 
@@ -97,6 +117,7 @@ class PostServiceTest {
                                .mapToObj(i -> Post.builder()
                                                   .title("잼미니 제목 " + i)
                                                   .content("내용입니다 " + i)
+                                                  .user(user)
                                                   .build())
                                .toList();
         postRepository.saveAll(posts);
