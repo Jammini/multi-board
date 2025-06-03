13555505fac2ffadb745b3f98c0627a4e268beeb
Jammini <systemdata227@gmail.com>
2025-06-03 21:17:42 +0900

    [refactor] 서브 모듈 구조로 변경

---
+++ board-admin-server/src/main/java/com/boardadminserver/BoardAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.boardadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardAdminServerApplication.class, args);
+    }
+
+}
---
+++ board-admin-server/src/test/java/com/boardadminserver/BoardAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
