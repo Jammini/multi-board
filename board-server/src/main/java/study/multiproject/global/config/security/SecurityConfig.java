b2652402d7d4d8947d49d424b32e0386d54a69d2
Jammini <systemdata227@gmail.com>
2025-06-13 18:23:15 +0900

    [env] 사용하지 않은 import 문 제거

--- board-server/src/main/java/study/multiproject/global/config/security/SecurityConfig.java
+++ board-server/src/main/java/study/multiproject/global/config/security/SecurityConfig.java
@@ -1,12 +1,9 @@
 package study.multiproject.global.config.security;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
