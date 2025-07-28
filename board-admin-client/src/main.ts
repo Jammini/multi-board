d598f7ddc288bf4d5b6b4004713f76dad08ae4dd
Jammini <systemdata227@gmail.com>
2025-07-28 16:25:12 +0900

    [feature] 게시판 통계 관리자 화면

--- board-admin-client/src/main.ts
+++ board-admin-client/src/main.ts
@@ -9,10 +9,33 @@ import 'element-plus/dist/index.css'
 
 import "bootstrap/dist/css/bootstrap-utilities.css"
 
+import VueECharts from 'vue-echarts'
+import 'echarts'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { PieChart, BarChart } from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent
+} from 'echarts/components'
+
+
+use([
+  CanvasRenderer,
+  PieChart,
+  BarChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent
+])
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
-
+app.component('v-chart', VueECharts)
 app.mount('#app')
